78d7111bb8494f9e9935f79064c5e3344c0689ab
PTRBRNR <ptrbrnr@rocketmail.com>
2022-03-23 13:45:58 +0100

    :tada: 1:34 min before nextauth works

---
+++ backupconfig.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
+ // "watch": "./node_modules/.bin/postcss ./styles/globals.css -o ./public/out.css --watch"
